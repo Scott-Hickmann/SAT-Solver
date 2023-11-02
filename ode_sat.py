b763c2f20e1828d715e544adba2784811f961363
Scott Hickmann <scotthickmann@DN0a22a26e.SUNet>
2023-11-02 00:32:48 -0700

    Format code

--- ode_sat.py
+++ ode_sat.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 
 np.random.seed(0)
 
-class OdeSat:
 
+class OdeSat:
     def __init__(self, clauses: np.ndarray, resolution, time):
         self.resolution = resolution
         self.time = time
@@ -19,7 +19,7 @@ class OdeSat:
 
         print(self.c)
         self.cT = self.c.T
-        
+
         # self.c = np.array([
         #     [1, 1, -1],
         #     [1, -1, 1],
@@ -28,26 +28,26 @@ class OdeSat:
         #     [-1, 1, -1],
         #     [-1, -1, 1]
         # ])
-        
+
         # M clauses, I variables
         self.M, self.I = self.c.shape
 
         print(f"M: {self.M}, I: {self.I}")
 
-        self.initial_s = np.random.uniform(-1,1, self.I)
+        self.initial_s = np.random.uniform(-1, 1, self.I)
         self.initial_a = self.time * np.ones(self.M)
         self.initial = np.concatenate((self.initial_s, self.initial_a))
 
     def K(self, s, m):
         return np.prod(1 - self.c[m] * s)
-    
+
     # def Ki(self, K_vals, s, m, i):
     #     return K_vals[m] / (1 - self.c[m, i] * s[i])
-    
+
     def V(self, s, a):
         K_vals = np.array([self.K(s, m) for m in range(self.M)])
-        return a @ (K_vals ** 2)
-    
+        return a @ (K_vals**2)
+
     # def ds_i(self, s, a, i, K_vals):
     #     c_vals = self.c[:, i]
     #     denominator = 1 - (c_vals * s[i])
@@ -59,18 +59,18 @@ class OdeSat:
 
     def da(self, a, K_vals):
         return a * K_vals
-    
+
     def ds(self, s, a, K_vals):
         s_broadcasted = s[:, np.newaxis]
         denominator = 1 - (self.cT * s_broadcasted)
         Ki_vals = K_vals / denominator
-        intermediate_result = (self.cT * Ki_vals * K_vals)
+        intermediate_result = self.cT * Ki_vals * K_vals
         result = 2 * np.sum(a * intermediate_result, axis=1)
         return result
 
     def derivative(self, state, t):
         # print(f"t: {t}")
-        s, a = state[:self.I], state[self.I:]
+        s, a = state[: self.I], state[self.I :]
         K_vals = np.array([self.K(s, m) for m in range(self.M)])
         ds = self.ds(s, a, K_vals)
         da = self.da(a, K_vals)
@@ -88,10 +88,12 @@ class OdeSat:
         timeseries = np.empty((len(t), len(self.initial)))
         timeseries[0] = self.initial
         for i in range(1, len(t)):
-            timeseries[i] = timeseries[i-1] + dt * self.derivative(timeseries[i-1], t[i-1])
+            timeseries[i] = timeseries[i - 1] + dt * self.derivative(
+                timeseries[i - 1], t[i - 1]
+            )
 
-        sTs = timeseries[:,:self.I]
-        aTs = timeseries[:,self.I:]
+        sTs = timeseries[:, : self.I]
+        aTs = timeseries[:, self.I :]
 
         return sTs, aTs
 
@@ -102,6 +104,7 @@ class OdeSat:
 
 if __name__ == "__main__":
     from pysat.formula import CNF
+
     resolution = 1000
     duration = 1
     file_name = "coloring_basic"
@@ -109,6 +112,7 @@ if __name__ == "__main__":
     result_name = f"{file_name}_euler_{duration}s_{resolution}res_3"
 
     import time
+
     ode_sat = OdeSat(clauses=f1.clauses, resolution=resolution, time=duration)
     start = time.time()
     sTs, aTs = ode_sat.integrate()
@@ -119,9 +123,9 @@ if __name__ == "__main__":
     print([index + 1 if value > 0 else -index - 1 for index, value in enumerate(last)])
     plt.plot(sTs)
     plt.ylim(-1.1, 1.1)
-    plt.legend(np.arange(ode_sat.I)+1)
+    plt.legend(np.arange(ode_sat.I) + 1)
     plt.savefig(f"out/{result_name}.png")
-    
+
     plt.clf()
     vs = [ode_sat.V(s, a) for s, a in zip(sTs, aTs)]
     plt.plot(vs)
