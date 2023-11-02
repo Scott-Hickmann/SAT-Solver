34c0673cce39f2ad59a5cca9336d77eedf00cee2
Elijah Kim <reachelijah@gmail.com>
2023-11-02 00:53:40 -0700

    optimizations, bug fixes

--- ode_sat.py
+++ ode_sat.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 
-np.random.seed(0)
+np.random.s
 
 
 class OdeSat:
@@ -18,6 +18,7 @@ class OdeSat:
                 self.c[clause_index][abs(variable) - 1] = 1 if variable > 0 else -1
 
         # print(self.c)
+        self.cT = self.c.T
 
         # self.c = np.array([
         #     [1, 1, -1],
@@ -33,70 +34,78 @@ class OdeSat:
 
         print(f"M: {self.M}, I: {self.I}")
 
-        self.initial_s = np.random.uniform(-1, 1, self.I)
+        self.initial_s = rng.uniform(-1, 1, self.I)
         self.initial_a = self.time * np.ones(self.M)
         self.initial = np.concatenate((self.initial_s, self.initial_a))
+        print("initial:", np.max(self.initial))
 
-    def K(self, s, a, m, i=None):
-        c_vals = np.array(self.c)[m]
-        if i is not None:
-            c_vals[i] = 0
-        out = np.prod(1 - c_vals * s)
-        return out
+    def K(self, s, m):
+        return np.prod(1 - self.c[m] * s)
+
+    # def Ki(self, K_vals, s, m, i):
+    #     return K_vals[m] / (1 - self.c[m, i] * s[i])
 
     def V(self, s, a):
-        K_vals = np.array([self.K(s, a, m) for m in range(self.M)])
+        K_vals = np.array([self.K(s, m) for m in range(self.M)])
         return a @ (K_vals**2)
 
-    def ds_i(self, s, a, i):
-        c_vals = np.array(self.c)[:, i]
-        K_vals_without_i = np.array([self.K(s, a, m) for m in range(self.M)])
-        K_vals_with_i = np.array([self.K(s, a, m, i) for m in range(self.M)])
-        return np.sum(2 * a * c_vals * K_vals_with_i * K_vals_without_i)
+    # def ds_i(self, s, a, i, K_vals):
+    #     c_vals = self.c[:, i]
+    #     denominator = 1 - (c_vals * s[i])
+    #     Ki_vals = K_vals / denominator
+    #     return 2 * a @ (c_vals * Ki_vals * K_vals)
 
-    def da_m(self, s, a, m):
-        return a[m] * self.K(s, a, m)
+    # def da_m(self, K_vals, a, m):
+    #     return a[m] * K_vals[m]
+
+    def da(self, a, K_vals):
+        return a * K_vals
+
+    def ds(self, s, a, K_vals):
+        s_broadcasted = s[:, np.newaxis]
+        denominator = 1 - (self.cT * s_broadcasted)
+        Ki_vals = K_vals / denominator
+        intermediate_result = self.cT * Ki_vals * K_vals
+        result = 2 * np.sum(a * intermediate_result, axis=1)
+        return result
 
     def derivative(self, state):
         # print(f"t: {t}")
         s, a = state[: self.I], state[self.I :]
-        ds = np.array([self.ds_i(s, a, i) for i in range(len(s))])
-        da = np.array([self.da_m(s, a, m) for m in range(len(a))])
+        K_vals = np.array([self.K(s, m) for m in range(self.M)])
+        ds = self.ds(s, a, K_vals)
+        da = self.da(a, K_vals)
 
-        d = np.concatenate((ds, da))
-        return d
+        return np.concatenate((ds, da))
 
     def integrate(self):
-        try:
-            t = np.linspace(0, self.time, self.resolution)
+        t = np.linspace(0, self.time, self.resolution)
 
-            # ODE Int
-            # timeseries = odeint(self.derivative, self.initial, t)
+        # ODE Int
+        # timeseries = odeint(self.derivative, self.initial, t)
 
-            # Euler
-            dt = t[1] - t[0]
-            timeseries = np.empty((len(t), len(self.initial)))
-            timeseries[0] = self.initial
-            # print("t shape", timeseries.shape)
-            for i in range(1, len(t)):
-                timeseries[i] = timeseries[i - 1] + dt * self.derivative(
-                    timeseries[i - 1]
-                )
+        # Euler
+        dt = t[1] - t[0]
+        timeseries = np.empty((len(t), len(self.initial)))
+        timeseries[0] = self.initial
+        print("initial", self.initial)
+        # print("t shape", timeseries.shape)
+        for i in range(1, len(t)):
+            timeseries[i] = timeseries[i - 1] + dt * self.derivative(timeseries[i - 1])
 
-                if np.isnan(timeseries).any() or np.any(
-                    (timeseries < -1.5) | (timeseries > 1.5)
-                ):
-                    # print("stiff")
-                    print(np.isnan(timeseries).any())
-                    raise Exception(f"Stiff ODE. Step size is too large.")
+            sTs = timeseries[i, : self.I]
 
-            sTs = timeseries[:, : self.I]
-            aTs = timeseries[:, self.I :]
+            if np.isnan(sTs).any() or np.any((sTs < -1.2) | (sTs > 1.2)):
+                # print("stiff")
+                print(np.isnan(sTs).any())
+                print("i:", i)
+                print(np.nanmax(sTs, 0))
+                raise Exception(f"Stiff ODE. Step size is too large.")
 
-            return sTs, aTs
-        except Exception as e:
-            print("exception occurred", e)
-            raise
+        sTs = timeseries[:, : self.I]
+        aTs = timeseries[:, self.I :]
+
+        return sTs, aTs
 
     def run(self):
         timeseries = self.integrate()
@@ -104,14 +113,16 @@ class OdeSat:
 
 
 def test_stiffness(clauses, time_limit=1):
+    global rng
     res = 10
     while res < 10000000:
         print(res)
         try:
-            np.random.seed(0)
+            rng = np.random.default_rng(0)
             ode = OdeSat(clauses=clauses, resolution=res, time=time_limit)
             ts = ode.integrate()
-        except:
+        except Exception as e:
+            print("NEXT")
             res = int(res * 1.2)
             continue
         print("FOUND WORKING RES:", res)
@@ -128,7 +139,7 @@ if __name__ == "__main__":
     res = test_stiffness(f1.clauses)
     print("resolution", res)
 
-    np.random.seed(0)
+    rng = np.random.default_rng(0)
     ode_sat = OdeSat(clauses=f1.clauses, resolution=res, time=1)
     sTs, aTs = ode_sat.integrate()
 
@@ -136,7 +147,7 @@ if __name__ == "__main__":
     last = sTs[-1]
     print(last.tolist())
     print([index + 1 if value > 0 else -index - 1 for index, value in enumerate(last)])
-    print("Final resolution:", res)
+    # print("Final resolution:", res)
     plt.plot(sTs)
     plt.ylim(-1.1, 1.1)
     plt.legend(np.arange(ode_sat.I) + 1)
