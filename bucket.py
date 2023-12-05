fdf2af7856c130802a66f86f4e4806a3a559d833
Elijah Kim <reachelijah@gmail.com>
2023-12-05 00:33:54 -0800

    leaky buckets

--- bucket.py
+++ bucket.py
@@ -1,10 +1,13 @@
 import numpy as np
 from pysat.formula import CNF
+from matplotlib import pyplot as plt
+from solver import count_solved
 
 
 class Bucket:
     def __init__(self, initial_size=500, initial_value=0.5):
         self.size = initial_size
+        self.kept_rate = 0.999
         self.bucket = list(
             np.random.choice(
                 [0, 1], size=initial_size, p=[1 - initial_value, initial_value]
@@ -18,13 +21,18 @@ class Bucket:
         b = 0
         while len(self.bucket) > self.size:
             b = self.bucket.pop(np.random.randint(len(self.bucket)))
+        self.leak()
         return b
+
+    def leak(self):
+        self.size *= self.kept_rate
+        self.bucket = self.bucket[: int(self.size)]
 
     def get_value(self) -> float:
         return sum(self.bucket) / len(self.bucket)
 
     def __repr__(self) -> str:
-        return f"Bucket {self.get_value()} ({sum(self.bucket)} / {len(self.bucket)})"
+        return f"Bucket {np.round(self.get_value(), 2)} ({sum(self.bucket)} / {len(self.bucket)})"
 
 
 def term_unsatisfied(variable_normal: bool, value: bool):
@@ -46,15 +54,19 @@ def get_new_values(
     clauses_unsatisfied: list, clauses_variables: list, value: bool, variable: int
 ):
     updated_variable_pool = []
+    a_clause_unsatisfied = False
     for clause_variables, clause_unsatisfied in zip(
         clauses_variables, clauses_unsatisfied
     ):
+        if clause_unsatisfied:
+            a_clause_unsatisfied = True
         if variable in clause_variables:
             updated_variable_pool.append(clause_unsatisfied ^ value)
-    return updated_variable_pool
+    return updated_variable_pool, not a_clause_unsatisfied
 
 
-problem = CNF(from_file="test_files/complex.cnf")
+problem_file = "test_files/coloring_basic.cnf"
+problem = CNF(from_file=problem_file)
 
 num_variables = problem.nv
 clauses = [[variable > 0 for variable in clause] for clause in problem.clauses]
@@ -62,7 +74,7 @@ clauses_variables = [
     [abs(variable) - 1 for variable in clause] for clause in problem.clauses
 ]
 
-buckets = [Bucket() for _ in range(num_variables)]
+buckets = [Bucket(initial_size=500) for _ in range(num_variables)]
 
 
 def update():
@@ -75,24 +87,39 @@ def update():
         ]
         clauses_unsatisfied.append(clause_unsatisfied(terms_unsatisfied))
     for variable, value in enumerate(values):
-        new_values = get_new_values(
+        new_values, done = get_new_values(
             clauses_unsatisfied, clauses_variables, value, variable
         )
         for new_value in new_values:
             buckets[variable].add_bit(new_value)
 
+    return np.any([bucket.size == 1 for bucket in buckets])
 
-print([bucket for bucket in buckets])
+
+history = []
 for i in range(100000):
-    update()
-    if i % 1000 == 0:
+    done = update()
+    if done:
+        print(f"\nfinished in {i} iterations")
+        break
+    history.append([bucket.get_value() for bucket in buckets])
+    if i % 2000 == 0:
         print([bucket for bucket in buckets])
+print([bucket for bucket in buckets])
 
 result = [
     variable + 1 if round(bucket.get_value()) > 0 else -(variable + 1)
     for variable, bucket in enumerate(buckets)
 ]
 print(result)
+print(f"clauses correct: {count_solved(problem_file, result)} / {len(problem.clauses)}")
+
+plt.plot(history)
+plt.ylim([-0.1, 1.1])
+plt.legend(range(1, len(buckets) + 1))
+savepath = f"out/{problem_file.split('/')[-1][:-4]}.png"
+plt.savefig(savepath)
+print("Saved to " + savepath)
 
 """
 if all termSatisfied is False, then we need to "kick" the system
--- solver.py
+++ solver.py
@@ -11,6 +11,21 @@ def solve(file_name: str):
         return fm.model
 
 
+def count_solved(file_name: str, result: list):
+    f1 = CNF(from_file=file_name)
+    correct_clauses = 0
+    for clause in f1.clauses:
+        clause_result = False
+        for variable in clause:
+            value = result[abs(variable) - 1] > 0
+            if variable > 0:
+                clause_result = clause_result or value
+            else:
+                clause_result = clause_result or not value
+        correct_clauses += 1 if clause_result else 0
+    return correct_clauses
+
+
 def verify(file_name: str, result: list):
     f1 = CNF(from_file=file_name)
     for clause in f1.clauses:
@@ -29,8 +44,31 @@ def verify(file_name: str, result: list):
 if __name__ == "__main__":
     print(
         verify(
-            "test_files/complex.cnf",
-            [-1, 2, -3, 4, -5, -6, -7, -8, -9, -10, -11, 12, -13, -14, -15],
+            "test_files/coloring_basic.cnf",
+            [
+                -1,
+                -2,
+                3,
+                4,
+                -5,
+                -6,
+                -7,
+                -8,
+                -9,
+                10,
+                11,
+                -12,
+                -13,
+                14,
+                -15,
+                -16,
+                17,
+                18,
+                19,
+                -20,
+                -21,
+                22,
+            ],
         )
     )
     # soln = solve("test_files/complex.cnf")
